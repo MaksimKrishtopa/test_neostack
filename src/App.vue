50509a8981c705581fd23bc89814b49735445c04
MaksimKrishtopa <k.maks21@mail.ru>
2024-02-28 03:53:27 +0700

    Создание компонента и представления приложения

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <template>
-
+  <div id="app">
+    <DateRangeSliderView />
+  </div>
 </template>
+
+<script>
+import DateRangeSliderView from "@/views/DateRangeSliderView.vue";
+
+export default {
+  components: {
+    DateRangeSliderView,
+  },
+};
+</script>
 
 <style>
 #app {
-
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  background-color: #e0e0e0;
+  padding: 50px;
 }
 
+h1 {
+  color: #2196f3;
+}
 </style>
---
+++ src/views/DateRangeSliderView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Date Range Slider</h1>
+    <DateRangeSlider
+      :minYear="minYear"
+      :maxYear="maxYear"
+      @updateRange="handleRangeUpdate"
+    />
+  </div>
+</template>
+
+<script>
+import DateRangeSlider from "@/components/DateRangeSlider.vue";
+
+export default {
+  components: {
+    DateRangeSlider,
+  },
+  data() {
+    return {
+      minYear: 2014,
+      maxYear: 2021,
+    };
+  },
+
+};
+</script>
